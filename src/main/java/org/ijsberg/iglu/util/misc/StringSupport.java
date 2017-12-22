5f6d9f3cb3ce224005a08c71842d9bcd96ecd7b3
Jeroen Meetsma <jeroen@ijsberg.nl>
2017-12-22 14:08:43 +0100

    fixed stack trace reporting

--- src/main/java/org/ijsberg/iglu/util/misc/StringSupport.java
+++ src/main/java/org/ijsberg/iglu/util/misc/StringSupport.java
@@ -975,7 +975,7 @@ public abstract class StringSupport {
 	public static String getStackTrace(Throwable t, int depth, String prefix) {
 		StringBuffer retval = new StringBuffer();
 		int nrWritten = 0;
-		retval.append(t.toString() + " with message: " + t.getMessage());
+		retval.append(t.toString());
 		StackTraceElement[] elements = t.getStackTrace();
 		for (int i = 0; nrWritten < depth && i < elements.length; i++) {
 			String line = elements[i].toString();
