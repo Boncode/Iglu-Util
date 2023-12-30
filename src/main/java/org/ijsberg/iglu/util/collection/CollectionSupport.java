3c33b3fff36bdce5ebe7e303ce46e7e04dc11d9f
Jeroen Meetsma <jeroen@ijsberg.nl>
2023-12-30 19:49:24 +0100

    Added number of entanglement metrics

--- src/main/java/org/ijsberg/iglu/util/collection/CollectionSupport.java
+++ src/main/java/org/ijsberg/iglu/util/collection/CollectionSupport.java
@@ -195,7 +195,7 @@ public abstract class CollectionSupport {
 			value = 0;
 
 		}
-		map.put(key, Integer.valueOf(value.intValue() + increment.intValue()));
+		map.put(key, Integer.valueOf(value.intValue() + (increment == null ? 0 : increment.intValue())));
 	}
 
 	public static <K, V extends Long> void incrementLongByKey(Map<K, Long> map, K key, V increment) {
@@ -204,7 +204,7 @@ public abstract class CollectionSupport {
 			value = 0l;
 
 		}
-		map.put(key, Long.valueOf(value.longValue() + increment.longValue()));
+		map.put(key, Long.valueOf(value.longValue() + (increment == null ? 0l : increment.longValue())));
 	}
 
 	public static <K, V extends Float> void incrementFloatByKey(Map<K, Float> map, K key, V increment) {
@@ -213,7 +213,7 @@ public abstract class CollectionSupport {
 			value = 0.0f;
 
 		}
-		map.put(key, Float.valueOf(value.floatValue() + increment.floatValue()));
+		map.put(key, Float.valueOf(value.floatValue() + (increment == null ? 0f: increment.floatValue())));
 	}
 
 	public static <K, V extends Number> void incrementNumberByKey(Map<K, V> map, K key, V increment) {
@@ -230,16 +230,16 @@ public abstract class CollectionSupport {
 			}
 
 		}
-		if(value instanceof Integer) {
-			map.put(key, (V) Integer.valueOf((value).intValue() + increment.intValue()));
+		if(increment instanceof Integer) {
+			map.put(key, (V) Integer.valueOf((value).intValue() + (increment == null ? 0 : increment.intValue())));
 			return;
 		}
-		if(value instanceof Float) {
-			map.put(key, (V) Float.valueOf(value.floatValue() + increment.floatValue()));
+		if(increment instanceof Float) {
+			map.put(key, (V) Float.valueOf(value.floatValue() + (increment == null ? 0f: increment.floatValue())));
 			return;
 		}
-		if(value instanceof Long) {
-			map.put(key, (V) Long.valueOf((value).longValue() + increment.longValue()));
+		if(increment instanceof Long) {
+			map.put(key, (V) Long.valueOf((value).longValue() + (increment == null ? 0l : increment.longValue())));
 			return;
 		}
 		throw new IllegalArgumentException("type " + value.getClass().getSimpleName() + " not supported");
