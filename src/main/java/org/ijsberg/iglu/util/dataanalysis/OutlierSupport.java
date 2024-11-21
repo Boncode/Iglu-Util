323d64e837ed286908b9f6b7959c7f99cbdc9ace
Koen Naarding <koen_naarding97@hotmail.com>
2024-11-21 13:00:56 +0100

    1 spacing change to trigger initial push of new integration branch

--- src/main/java/org/ijsberg/iglu/util/dataanalysis/OutlierSupport.java
+++ src/main/java/org/ijsberg/iglu/util/dataanalysis/OutlierSupport.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class OutlierSupport {
 
-
     public static boolean naiveValueFitsTrend(float currentValue, List<Float> trend, float maxPercentageOffset) {
         float minVal = trend.get(trend.size()-1) * (1 - (maxPercentageOffset/100));
         float maxVal = trend.get(trend.size()-1) * (1 + (maxPercentageOffset/100));
